00e9e8b197906a9aa0d59a49c7443e176319231d
Aarsh Patel <aarsh18patel@gmail.com>
2024-04-30 15:33:24 +0530

    final commit

--- exception_handling_concept.py
+++ exception_handling_concept.py
@@ -15,18 +15,31 @@
 
 # Exception Handling
 
-try:
-    file = open("a_file.txt")
-    dictionary = {"key": "value"}
-    print(dictionary["not_existing_key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Hello World!")
-except KeyError as not_existing_key:
-    print(f"The key {not_existing_key} is not existing.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed.")
+# try:
+#     file = open("a_file.txt")
+#     dictionary = {"key": "value"}
+#     print(dictionary["not_existing_key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Hello World!")
+# except KeyError as not_existing_key:
+#     print(f"The key {not_existing_key} is not existing.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed.")
+#     raise TypeError("This is an error that I made up.")
+
+
+# BMI Example
+
+# height = float(input("Height in meters: "))
+# weight = int(input("Weight: "))
+#
+# if height > 3:
+#     raise ValueError("Human Height should not be over 3 meters.")
+#
+# bmi = weight / height ** 2
+# print(bmi)
